9c7a71b0b87c6da06e8e3e742b6223f7cc47bb3f
NAST <bibashchaudhary80@gmail.com>
2025-06-06 16:07:07 +0545

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,26 @@
+// fibpnacci series using array.
+#include<stdio.h>
+int fib(int);
+int main()
+{
+	int n,i;
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	printf("Fibonacci series: ");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",fib(i));
+	}
+	return 0;
+}
+int fib(int n)
+{
+	if (n<=1)
+	{
+		return 1;
+	}
+	else
+	{
+		return fib(n-1)+fib(n-2);
+	}
+}
